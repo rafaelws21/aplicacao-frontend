e31b03fad46432bae9ac6fa6920a783f33068acd
rafael.sousa <rafael.sousa@ctis.com.br>
2017-02-17 13:39:36 -0200

    inclusão de diretorio

--- app/config/express.js
+++ app/config/express.js
@@ -8,7 +8,18 @@ var express = require('express');
 module.exports = function(){
 
     var app = express();
+
+    //configuração de ambiente
     app.set('port', 3000);
+
+    //middleware
+    app.use(express.static('./public'));
+
+    //abaixo do middleware express.static
+
+    app.set('view engine', 'ejs');
+    app.set('views','./app/views');
+    
     return app;
 };
 
